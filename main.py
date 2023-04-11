1390ace618b7ef941534fbf987018098c44156cc
StasPeretolchin <peretolchin@defeval.net>
2023-04-11 12:15:09 +0600

    Refactoring

--- main.py
+++ main.py
@@ -23,11 +23,6 @@ def move_file(initial_path, final_path):
 
 
 def main():
-    print(EXCEL_DIR)
-    print(WORD_DIR)
-    print(OUTPUT_DIR)
-    print(TRASH_DIR)
-
     while True:
         timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_")
         for file in os.scandir(EXCEL_DIR):
@@ -37,7 +32,6 @@ def main():
 
             if file_ext.replace('.', '') not in TYPE_LIST:
                 print('файл не в списке разрешенных расширений')
-                move_file(EXCEL_DIR + file.name, TRASH_DIR + timestamp + file.name)
             else:
                 print('Запускаем распознавание!')
                 cmd = fr'chcp 1251 & "{ABBYY_PATH}" "{EXCEL_DIR}{file.name}" /lang English Russian ' \
@@ -45,7 +39,8 @@ def main():
                 print(cmd)
                 exit_code = os.system(cmd)
                 print("`FineCmd.exe ~` run with exit code %d" % exit_code)
-                move_file(EXCEL_DIR + file.name, TRASH_DIR + timestamp + file.name)
+
+            move_file(EXCEL_DIR + file.name, TRASH_DIR + timestamp + file.name)
 
 
 if __name__ == "__main__":
